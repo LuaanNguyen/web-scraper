c69ebf25a4a6444d1e665582cdc0cee25c9de05d
LuaanNguyen <luanthiennguyen2003@gmail.com>
2025-04-03 20:08:39 -0700

    Add annotations to run on Modal

--- scrape.py
+++ scrape.py
@@ -1,18 +1,29 @@
+# Modal run scrape.py --url http://example.com
+# Modal automatically parse the url parameter into main(url)
 import re 
 import sys 
 import urllib.request 
+import modal 
 
+app = modal.App(name="link-scraper") #create an app called link-scrapper
+
+@app.function()
 def get_links(url):
     response = urllib.request.urlopen(url)
     html = response.read().decode("utf8")
     
     links = [] 
     
-    for match in re.finditer('href="(.*?)"', html):
+    for match in re.finditer('href="(.*?)"', html): # find all strings that match the HTML hypterlink pattern
         links.append(match.group(1))
         
     return links 
 
-if __name__ == "__main__":
-    links = get_links(sys.argv[1])
+
+@app.local_entrypoint()
+def main(url):
+    links = get_links.remote(url)
     print(links)
+
+
+   
